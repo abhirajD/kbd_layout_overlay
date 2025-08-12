6c4cf662f0f3de262ea789f3af4f11c75cd56aa4
Abhiraj D <abhiraj4u@hotmail.com>
2025-08-11 18:18:34 -0700

    Add tray menus and autostart configuration

--- windows/main.c
+++ windows/main.c
@@ -1,30 +1,44 @@
 #include <windows.h>
+#include <shellapi.h>
 #include <stdio.h>
 #include <string.h>
+#include "../shared/config.h"
 #include "../shared/overlay.h"
 
 static Overlay g_overlay;
 static HBITMAP g_bitmap;
 static HWND g_hwnd;
+static Config g_cfg;
+static NOTIFYICONDATAA g_nid;
+static char g_cfg_path[MAX_PATH] = "config.cfg";
+#define WM_TRAY (WM_APP + 1)
 
-static void register_autostart(void) {
+static void set_autostart(int enable) {
     HKEY key;
     if (RegCreateKeyExA(HKEY_CURRENT_USER,
             "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, NULL, 0,
             KEY_SET_VALUE, NULL, &key, NULL) == ERROR_SUCCESS) {
-        char path[MAX_PATH];
-        GetModuleFileNameA(NULL, path, MAX_PATH);
-        RegSetValueExA(key, "kbd_layout_overlay", 0, REG_SZ,
-            (const BYTE *)path, (DWORD)(strlen(path) + 1));
+        if (enable) {
+            char path[MAX_PATH];
+            GetModuleFileNameA(NULL, path, MAX_PATH);
+            RegSetValueExA(key, "kbd_layout_overlay", 0, REG_SZ,
+                (const BYTE *)path, (DWORD)(strlen(path) + 1));
+        } else {
+            RegDeleteValueA(key, "kbd_layout_overlay");
+        }
         RegCloseKey(key);
     }
 }
 
 static int init_bitmap(void) {
-    if (load_overlay_image("keymap.png", &g_overlay) != 0) {
-        MessageBoxA(NULL, "Failed to load keymap.png", "Error", MB_OK);
+    const char *path = g_cfg.overlay_path[0] ? g_cfg.overlay_path : "keymap.png";
+    if (load_overlay_image(path, &g_overlay) != 0) {
+        char msg[256];
+        sprintf(msg, "Failed to load %s", path);
+        MessageBoxA(NULL, msg, "Error", MB_OK);
         return 0;
     }
+    apply_opacity_inversion(&g_overlay, g_cfg.opacity, g_cfg.invert);
 
     BITMAPV5HEADER bi = {0};
     bi.bV5Size = sizeof(BITMAPV5HEADER);
@@ -68,6 +82,17 @@ static void update_window(void) {
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
+    case WM_TRAY:
+        if (lParam == WM_RBUTTONUP) {
+            HMENU menu = CreatePopupMenu();
+            AppendMenuA(menu, MF_STRING | (g_cfg.autostart ? MF_CHECKED : 0), 1, "Start at login");
+            AppendMenuA(menu, MF_STRING, 2, "Quit");
+            POINT p; GetCursorPos(&p);
+            SetForegroundWindow(hwnd);
+            TrackPopupMenu(menu, TPM_LEFTALIGN | TPM_BOTTOMALIGN, p.x, p.y, 0, hwnd, NULL);
+            DestroyMenu(menu);
+        }
+        break;
     case WM_HOTKEY:
         if (wParam == 1) {
             static int visible = 0;
@@ -75,7 +100,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             ShowWindow(hwnd, visible ? SW_SHOW : SW_HIDE);
         }
         break;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == 1) {
+            g_cfg.autostart = !g_cfg.autostart;
+            set_autostart(g_cfg.autostart);
+            save_config(g_cfg_path, &g_cfg);
+        } else if (LOWORD(wParam) == 2) {
+            PostMessage(hwnd, WM_CLOSE, 0, 0);
+        }
+        break;
     case WM_DESTROY:
+        Shell_NotifyIconA(NIM_DELETE, &g_nid);
         UnregisterHotKey(NULL, 1);
         PostQuitMessage(0);
         break;
@@ -84,7 +119,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nShow) {
-    register_autostart();
+    if (load_config(g_cfg_path, &g_cfg) != 0) {
+        strcpy(g_cfg.overlay_path, "keymap.png");
+        g_cfg.opacity = 1.0f;
+        g_cfg.invert = 0;
+        g_cfg.autostart = 0;
+        save_config(g_cfg_path, &g_cfg);
+    }
+    set_autostart(g_cfg.autostart);
 
     if (!init_bitmap()) {
         return 0;
@@ -102,6 +144,15 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nShow)
 
     update_window();
     ShowWindow(g_hwnd, SW_HIDE);
+
+    g_nid.cbSize = sizeof(g_nid);
+    g_nid.hWnd = g_hwnd;
+    g_nid.uID = 1;
+    g_nid.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
+    g_nid.uCallbackMessage = WM_TRAY;
+    g_nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    lstrcpyA(g_nid.szTip, "Keyboard Layout Overlay");
+    Shell_NotifyIconA(NIM_ADD, &g_nid);
 
     RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT | MOD_SHIFT, VK_OEM_2);
 
